ac655bc121a84c2308df83f7fc93e94b43f7a46f
liyan2015 <li.yan.88@xjtu.edu.cn>
2023-06-18 01:06:38 +0800

    update test

--- canalenv/envs/__init__.py
+++ canalenv/envs/__init__.py
@@ -1,4 +1,1 @@
 from canalenv.envs.canalenv_gym import SumoEnv
-# from .vehicle import Vehicle
-# from .vehicleCollection import VehicleCollection#, vehicleCollection
-# import .vehicleCollection as vc
